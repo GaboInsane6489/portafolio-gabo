134238074c4f27aa6da4bfb1853f0213c81d4d4e
Gabriel Gonnzález <gabrielgg2005ve@gmail.com>
2025-06-30 19:55:57 -0400

    Limpieza: eliminando directorio Portafolio duplicado

--- Portafolio/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🌐 Servidor iniciado en http://localhost:${PORT}`);
-});
--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`🌐 Servidor iniciado en http://localhost:${PORT}`);
 });
