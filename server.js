7b419dfd7debabc40f9e0038b1060a17ebab3062
Gabriel Gonnzález <gabrielgg2005ve@gmail.com>
2025-06-30 20:02:57 -0400

    Ruta raíz agregada para Render

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ app.use(express.static("public"));
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🌐 Servidor iniciado en http://localhost:${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
